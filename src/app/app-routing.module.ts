3ed98b35060f782965dc26e0e338c39a88fda921
WafaWaheedaS <wafawaheeda@gmail.com>
2022-07-19 05:12:31 +0530

    Add stats for affected countries above map

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HistoryComponent } from './history/history.component';
 import { MapComponent } from './map/map.component';
-import { StatsComponent } from './stats/stats.component';
 
 const routes: Routes = [
   { path: 'map', component: MapComponent },
-  { path: 'stats', component: StatsComponent },
   { path: 'history', component: HistoryComponent },
   {path: '', redirectTo: 'map', pathMatch: 'full'}
 ];
--- src/app/stats/stats.component.ts
+++ src/app/stats/stats.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Subject, takeUntil } from 'rxjs';
-import { ChartConfiguration, ChartOptions } from "chart.js";
-import { Country, CountryStats } from '../shared/country.model';
-import { CountryService } from '../shared/country.service';
+import { ChartConfiguration } from "chart.js";
+import { CountryStats } from '../shared/country.model';
 import { StatsService } from '../shared/stats.service';
 
 @Component({
@@ -13,60 +12,84 @@ import { StatsService } from '../shared/stats.service';
 
 export class StatsComponent implements OnInit {
 
-  constructor(private statsService: StatsService, private countryService: CountryService) { }
+  constructor(private statsService: StatsService) { }
 
   selectedCountry!: string;
-  countryStats!: CountryStats;
+  countriesStats!: CountryStats[];
+  affectedCountries!: any[];
   _onDestroy = new Subject<void>();
 
-  public lineChartData: ChartConfiguration<'line'>['data'] = {
-    labels: [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
-      'July'
-    ],
-    datasets: [
-      {
-        data: [ 65, 59, 80, 81, 56, 55, 40 ],
-        label: 'Series A',
-        fill: true,
-        tension: 0.5,
-        borderColor: 'black',
-        backgroundColor: 'rgba(255,0,0,0.3)'
-      }
-    ]
+  public barChartLegend = true;
+  public barChartPlugins = [];
+
+  public barChartData!: ChartConfiguration<'bar'>['data'];
+  public barChartOptions: ChartConfiguration<'bar'>['options'] = {
+    responsive: false,
   };
-  public lineChartOptions: ChartOptions<'line'> = {
-    responsive: false
-  };
-  public lineChartLegend = true;
 
   ngOnInit(): void {
-    this.countryService.getSelectedCountry()
-    .pipe(takeUntil(this._onDestroy))
-    .subscribe((country: Country) => { if (country?.name) 
-        this.selectedCountry = country.name })
-        
-    this.getStatsForCountry();
+    this.getStatsForAllContries();
   }
-  
+
   ngOnDestroy() {
     this._onDestroy.next();
     this._onDestroy.complete();
   }
 
-  getStatsForCountry() {
-    if(this.selectedCountry){
-      this.statsService.getAll({ country: this.selectedCountry })      
-        .pipe(takeUntil(this._onDestroy))
-        .subscribe((countryStats: CountryStats) => {
-          this.countryStats = countryStats;
-      })
-    }
+  getStatsForAffectedCountries() {
+   this.affectedCountries?.forEach((country: any) => {
+    this.statsService.getAll({ country: this.selectedCountry })
+    .pipe(takeUntil(this._onDestroy))
+    .subscribe((countriesStats: CountryStats[]) => {
+    })
+   })
   }
 
+  getStatsForAllContries() {
+    this.statsService.getAll()
+      .pipe(takeUntil(this._onDestroy))
+      .subscribe((countriesStats: CountryStats[]) => {
+        this.countriesStats = countriesStats;
+        this.findMostAffectedCountries()
+      })
+
+  }
+
+  findMostAffectedCountries() {
+    let countriesTotalCases: any[] = [];
+    this.countriesStats?.forEach((stat) => countriesTotalCases.push({
+      'name': stat.name, 'total': stat.cases?.total, 'active': stat.cases?.active, 'recovered': stat.cases?.recovered
+    }))
+    countriesTotalCases = [...countriesTotalCases.sort((a, b) => b.total - a.total)]
+    this.affectedCountries = countriesTotalCases.slice(0, 5);
+    this.populateBarChartData()
+  }
+
+  populateBarChartData() {
+    let labels: string[] = [];
+    let totalCases: number[] = [];
+    let activeCases: number[] = [];
+    let recoveredCases: number[] = [];
+    this.affectedCountries.forEach(country => {
+      labels.push(country.name)
+      totalCases.push(country.total)
+      activeCases.push(country.active)
+      recoveredCases.push(country.recovered)
+    })
+   this.barChartData = {
+      labels: labels,
+      datasets: [
+        { data: totalCases, 
+          label: 'Total Cases',
+          backgroundColor: '#990052' },
+        { data: activeCases, 
+          label: 'Active Cases',
+          backgroundColor: '#ff99cf' },
+        { data: recoveredCases, 
+          label: 'Recovered Cases',
+          backgroundColor: '#3fb57e' }
+      ]
+    };
+  }
+  
 }
